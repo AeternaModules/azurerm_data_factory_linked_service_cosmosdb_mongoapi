90df4d2a07a6fcae60e54e5c20026010f9e45606
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,47 +1,42 @@
-output "data_factory_linked_service_cosmosdb_mongoapis" {
-  description = "All data_factory_linked_service_cosmosdb_mongoapi resources"
-  value       = azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis
-  sensitive   = true
-}
 output "data_factory_linked_service_cosmosdb_mongoapis_additional_properties" {
-  description = "List of additional_properties values across all data_factory_linked_service_cosmosdb_mongoapis"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_linked_service_cosmosdb_mongoapis, keyed the same as var.data_factory_linked_service_cosmosdb_mongoapis"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : k => v.additional_properties }
 }
 output "data_factory_linked_service_cosmosdb_mongoapis_annotations" {
-  description = "List of annotations values across all data_factory_linked_service_cosmosdb_mongoapis"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : v.annotations]
+  description = "Map of annotations values across all data_factory_linked_service_cosmosdb_mongoapis, keyed the same as var.data_factory_linked_service_cosmosdb_mongoapis"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : k => v.annotations }
 }
 output "data_factory_linked_service_cosmosdb_mongoapis_connection_string" {
-  description = "List of connection_string values across all data_factory_linked_service_cosmosdb_mongoapis"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : v.connection_string]
+  description = "Map of connection_string values across all data_factory_linked_service_cosmosdb_mongoapis, keyed the same as var.data_factory_linked_service_cosmosdb_mongoapis"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : k => v.connection_string }
   sensitive   = true
 }
 output "data_factory_linked_service_cosmosdb_mongoapis_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_linked_service_cosmosdb_mongoapis"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_linked_service_cosmosdb_mongoapis, keyed the same as var.data_factory_linked_service_cosmosdb_mongoapis"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : k => v.data_factory_id }
 }
 output "data_factory_linked_service_cosmosdb_mongoapis_database" {
-  description = "List of database values across all data_factory_linked_service_cosmosdb_mongoapis"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : v.database]
+  description = "Map of database values across all data_factory_linked_service_cosmosdb_mongoapis, keyed the same as var.data_factory_linked_service_cosmosdb_mongoapis"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : k => v.database }
 }
 output "data_factory_linked_service_cosmosdb_mongoapis_description" {
-  description = "List of description values across all data_factory_linked_service_cosmosdb_mongoapis"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : v.description]
+  description = "Map of description values across all data_factory_linked_service_cosmosdb_mongoapis, keyed the same as var.data_factory_linked_service_cosmosdb_mongoapis"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : k => v.description }
 }
 output "data_factory_linked_service_cosmosdb_mongoapis_integration_runtime_name" {
-  description = "List of integration_runtime_name values across all data_factory_linked_service_cosmosdb_mongoapis"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : v.integration_runtime_name]
+  description = "Map of integration_runtime_name values across all data_factory_linked_service_cosmosdb_mongoapis, keyed the same as var.data_factory_linked_service_cosmosdb_mongoapis"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : k => v.integration_runtime_name }
 }
 output "data_factory_linked_service_cosmosdb_mongoapis_name" {
-  description = "List of name values across all data_factory_linked_service_cosmosdb_mongoapis"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : v.name]
+  description = "Map of name values across all data_factory_linked_service_cosmosdb_mongoapis, keyed the same as var.data_factory_linked_service_cosmosdb_mongoapis"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : k => v.name }
 }
 output "data_factory_linked_service_cosmosdb_mongoapis_parameters" {
-  description = "List of parameters values across all data_factory_linked_service_cosmosdb_mongoapis"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : v.parameters]
+  description = "Map of parameters values across all data_factory_linked_service_cosmosdb_mongoapis, keyed the same as var.data_factory_linked_service_cosmosdb_mongoapis"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : k => v.parameters }
 }
 output "data_factory_linked_service_cosmosdb_mongoapis_server_version_is_32_or_higher" {
-  description = "List of server_version_is_32_or_higher values across all data_factory_linked_service_cosmosdb_mongoapis"
-  value       = [for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : v.server_version_is_32_or_higher]
+  description = "Map of server_version_is_32_or_higher values across all data_factory_linked_service_cosmosdb_mongoapis, keyed the same as var.data_factory_linked_service_cosmosdb_mongoapis"
+  value       = { for k, v in azurerm_data_factory_linked_service_cosmosdb_mongoapi.data_factory_linked_service_cosmosdb_mongoapis : k => v.server_version_is_32_or_higher }
 }
 
